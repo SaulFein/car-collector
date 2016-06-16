1c2696fd6818afe87879fb399c26b94134c8a275
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-06-16 11:30:27 -0700

    added current car feature

--- app/controllers/user-controller.js
+++ app/controllers/user-controller.js
@@ -5,7 +5,8 @@ module.exports = function(app) {
     const vm = this;
     vm.user = [];
     vm.cars = [];
-    vm.curCar = 0
+    vm.curPos = 0;
+    vm.curCar;
     vm.user = ['user'];
     vm.uae = false; //uae = user already exists
     vm.ip = false; //ip = invalid password
@@ -55,8 +56,8 @@ module.exports = function(app) {
   CarService.getCars(userId)
     .then(function(res) {
       vm.cars = res.data.data;
-      vm.curCar = vm.cars[vm.curCar];
-      console.log("This is cur Car " + vm.curCar);
+      vm.curCar = vm.cars.inventory[vm.curPos];
+      console.log("This is cur Car ", vm.curCar );
     }, function(err) {
       console.log(err);
     });
